4607fafd89940024b558c1e44e0ee570738796dd
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2022-07-08 09:11:07 -0400

    Fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
-import { defineConfig, searchForWorkspaceRoot } from 'vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +12,7 @@ const config = {
 	],
 
 	kit: {
-		
+		adapter: adapter()
 	},
 };
 

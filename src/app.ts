237cc322ef181bccae522a72b77f8b1dc0cea6a4
Garbg <83542885+CloudyWhale@users.noreply.github.com>
2022-07-10 12:54:55 -0400

    Format code with prettier

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
-import '../app.css'
+import '../app.css';
 import { onMount } from 'svelte';
 let darkmode = 'true';
-	onMount(() => {
-		var allElements = document.getElementsByTagName('*');
-		// @ts-ignore
-		for (let element of allElements) {
-			element.style.opacity = 1;
-		}
-		if (window.localStorage.getItem('darkmode') === null) {
-			window.localStorage.setItem('darkmode', 'true');
-			document.documentElement.classList.add('dark');
-		} else {
-			if (window.localStorage.getItem('darkmode') === 'true') {
-				document.documentElement.classList.add('dark');
-			}
-			console.log(window.localStorage.getItem('darkmode'));
-		}
-	});
-
-	function darkToggle() {
-		if (window.localStorage.getItem('darkmode') === 'true') {
-			window.localStorage.setItem('darkmode', 'false');
-			document.documentElement.classList.remove('dark');
-		} else if (window.localStorage.getItem('darkmode') === 'false') {
-			window.localStorage.setItem('darkmode', 'true');
-			document.documentElement.classList.add('dark');
-		}
+onMount(() => {
+	var allElements = document.getElementsByTagName('*');
+	// @ts-ignore
+	for (let element of allElements) {
+		element.style.opacity = 1;
 	}
+	if (window.localStorage.getItem('darkmode') === null) {
+		window.localStorage.setItem('darkmode', 'true');
+		document.documentElement.classList.add('dark');
+	} else {
+		if (window.localStorage.getItem('darkmode') === 'true') {
+			document.documentElement.classList.add('dark');
+		}
+		console.log(window.localStorage.getItem('darkmode'));
+	}
+});
+
+function darkToggle() {
+	if (window.localStorage.getItem('darkmode') === 'true') {
+		window.localStorage.setItem('darkmode', 'false');
+		document.documentElement.classList.remove('dark');
+	} else if (window.localStorage.getItem('darkmode') === 'false') {
+		window.localStorage.setItem('darkmode', 'true');
+		document.documentElement.classList.add('dark');
+	}
+}
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	assetsInlineLimit: '1',
 	server: {
 		fs: {
-		  // Allow serving files from one level up to the project root
-		  allow: ['static/']
-		  
+			// Allow serving files from one level up to the project root
+			allow: ['static/']
 		}
-	  }
+	}
 };
 export default config;

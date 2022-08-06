6e476faf85b367db7b48bb66349a7068c93d4f5a
Garbg <83542885+CloudyWhale@users.noreply.github.com>
2022-08-06 16:39:00 -0400

    Add simple 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,8 @@
+<script>
+import '../404.css'
+</script>
+<div class="flex flex-col items-center h-screen justify-center">
+<h1 class="text-9xl text-center">404</h1>
+<h2 class="text-5xl text-center">not found</h2>
+<h3 class="text-2xl text-center">visit <a href="./" class="underline">home</a></h3>
+</div>
